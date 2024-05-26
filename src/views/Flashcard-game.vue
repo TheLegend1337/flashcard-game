4c40876d5ddf059b24cd142ea5a38ec76251cbf3
RobertFilatow <rf@pocket-rocket.io>
2024-05-26 12:26:04 +0200

    toughts about how to manage game loop and phases

--- src/views/Flashcard-game.vue
+++ src/views/Flashcard-game.vue
@@ -1,7 +1,45 @@
 <script setup>
+
+//TODO:
+//Ich muss hier alle Phasen jeweils als Funktion umsetzen 
+//und dann muss ich auf veränderung der Phase im Store hören können: 
+//https://pinia.vuejs.org/core-concepts/state.html
+//dann kann der Game View auf Veränderungen der Phasen reagieren.
+//andere Komponenten können dann die Phase an verschiedenen Stellen ändern.
+
 // import Card from "../components/Card.vue";
 import GameOverlay from "../components/GameOverlay.vue";
 // import Matchfield from "../components/Matchfield.vue";
+
+//importiere roundsmanager Store 
+//import { useRoundsmanagerStore } from "@/stores/roundsmanager";
+const phase = 'GameStart'; //setze phase im roundsmanager auf GameStart
+switch (phase) {
+  case 'GameStart':
+    console.log('Setze Karten in Hand auf 0');
+    console.log('Setze Lebenspunkte von Held auf 100%');
+    console.log('Setze Willenskraft von Held auf 100%');
+    console.log('Lade Überschrift Spiel hat gestartet ');
+    console.log('Lade zufälliges Monster');
+    break;
+  case 'DrawCards':
+    console.log('Wenn Runde 0 dann Ziehe 5 Karten');
+    console.log('Wenn mehr als 0 Karten auf Hand ziehe nur eine Karte');
+    console.log('Wenn WK nicht voll, Erhöhe Willenskraft um 1');
+  case 'PlayCards':
+    console.log('Spieler spielt Karte aus');
+    break;
+  case 'EndTurn':
+    console.log('Spieler beendet seinen Zug');
+    break;
+  case 'EnemyPhase':
+    console.log('Monster greift an');
+    console.log('ändere Phase in DrawCards');
+    break;
+  default:
+    console.log(`Sorry, we are out of ${phase}.`);
+}
+
 </script>
 
 <template>
@@ -9,6 +47,31 @@ import GameOverlay from "../components/GameOverlay.vue";
     <GameOverlay />
   </main>
 </template>
+<script>
+/*
+import { useRoundsmanagerStore } from "@/stores/roundsmanager";
+// import Flashcards from './db.json';
+export default {
+  data() {
+    return {
+      store : useRoundsmanagerStore(),
+      flashcards: [],
+      question: "What is a card?",
+      response: "This is the Cards Response.",
+    };
+  },
+  mounted() {
+         this.store.changePhase('GameStart');
+  },
+  methods: {
+    playcard(){
+      this.store.decreaseWillpower(2);
+    }
+
+  }
+};*/
+</script>
+
 <style>
 .flashcard-game {
   background-image: url("../assets/environments/tempel_mit_baeumen_korrigiert.png");
