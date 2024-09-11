08602cb26a536cda7dd0310752145bdf59b598a4
TheLegend1337 <robert.filatow@web.de>
2024-09-11 21:40:09 +0200

    CON-939-Responsives Design in Tailwind

--- src/components/QuizBox.vue
+++ src/components/QuizBox.vue
@@ -1,13 +1,27 @@
 <!--Options API-->
 <template>
-  <div class="quiz-box">
-    <div class="question">
-      <p>Hier steht lorem ipsum dolor sit amet consetetur elitir text.</p>
+  <div class="container quiz-box">
+    <div
+      v-if="isQuestionVisible"
+      class="container question flex justify-items-center flex-col items-center"
+    >
+      <p class="text-xs sm:text-xs md:text-xs lg:text-lg">
+        Lorem ipsum dolor sit amet, consetetur elitir?
+      </p>
+      <ButtonPrimary class="" :clickHandler="showAnswer" label="Antwort" />
     </div>
-    <div class="answer">
-      <ButtonSecondary :clickHandler="again" label="nochmal" />
-      <ButtonSecondary :clickHandler="hard" label="Schwer" />
-      <ButtonPrimary :clickHandler="easy" label="Einfach" />
+    <div v-else class="container answer text-center m-6">
+      <p class="text-xs sm:text-xs md:text-base lg:text-lg">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, sanctus est Lorem ipsum dolor sit amet.
+      </p>
+      <div class="difficulty">
+        <ButtonSecondary :clickHandler="again" label="nochmal" />
+        <ButtonSecondary :clickHandler="hard" label="Schwer" />
+        <ButtonPrimary :clickHandler="easy" label="Einfach" />
+      </div>
     </div>
   </div>
 </template>
@@ -25,6 +39,7 @@ export default {
       //Komma statt Semicolon benutzen
       //z.b store: useWillpowerStore(),
       //Achtung Doppelpunkt' : '' statt ' = ',
+      isQuestionVisible: true,
     };
   },
   methods: {
@@ -43,13 +58,24 @@ export default {
 .quiz-box {
   width: 100%;
   height: 100%;
-  border: 3px solid black;
-  background-color: white;
-  grid-row-start: 4;
+  grid-row-start: 2;
   grid-row-end: 7;
-  grid-column-start: 3;
-  grid-column-end: 8;
+  grid-column-start: 4;
+  grid-column-end: 7;
   justify-self: start;
   z-index: 100;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  padding: 100px 50px;
+  background-image: url("@/assets/ui-components-backgrounds/background-pergament-quadratisch.png");
+}
+.question {
+}
+.answer {
 }
 </style>
